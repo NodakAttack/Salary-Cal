615ad814ce3022bb31031a32138509f52ae15440
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-07-19 13:32:46 -0500

    added submit button function, gets input from the html input fields

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,20 @@
 console.log("testing");
 
+// when you click the submit button
 
-function addEmployee(event);
-let firstName = 
+function addEmployee(event){
+let firstName = document.querySelector("#first-name").value;
+let lastName = document.querySelector("#last-name").value;
+let id = document.querySelector("#id").value;
+let title = document.querySelector("#title").value;
+let salary = document.querySelector("#annual-salary").value;
 
-function delete(event) {
+
+
+console.log(firstName, lastName, id, title, salary);
+
+}
+
+function deleteEmployee(event) {
     event.target.closest("tr").remove();
   }
