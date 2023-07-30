ed15d1095462d90aadad169933e8a310faae30fc
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-07-30 12:28:10 -0500

    moved salary total to its own <div> after the table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,7 @@ let salaryTotal = 0;
 // when you click the submit button
 
 function addEmployee(event){
+
 let firstName = document.querySelector("#first-name").value;
 let lastName = document.querySelector("#last-name").value;
 let id = document.querySelector("#id").value;
@@ -12,6 +13,7 @@ let title = document.querySelector("#title").value;
 let salary = Number(document.querySelector("#annual-salary").value);
 let employee = document.querySelector("#employee-table");
 let totalSalary = document.querySelector("#total-salary");
+
 employee.innerHTML += `
 <tr>
     <td>${firstName}</td> 
@@ -26,8 +28,10 @@ employee.innerHTML += `
 // salary adds to total salary, passed to html
 salaryTotal = salaryTotal + salary;
 totalSalary.innerHTML = `
-Salary Total = ${salaryTotal}
+Salary Total = $${salaryTotal}
 `
+
+
 
 console.log(firstName, lastName, id, title, salary);
 console.log("salary total", salaryTotal);
